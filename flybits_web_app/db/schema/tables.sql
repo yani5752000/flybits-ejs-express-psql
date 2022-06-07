a39a65f7596b81c6e53103b76ad5602c0f5cdf37
Farid Asadpour <asadpourfarid@gmail.com>
2022-06-07 17:56:19 -0400

    tables4

--- flybits_web_app/db/schema/tables.sql
+++ flybits_web_app/db/schema/tables.sql
@@ -9,6 +9,6 @@ CREATE TABLE branchess (
 CREATE TABLE promotions (
   id SERIAL PRIMARY KEY NOT NULL,
   branch_id INTEGER REFERENCES branches(id) ON DELETE CASCADE,
-  capption VARCHAR(255) NOT NULL,
+  caption VARCHAR(255) NOT NULL,
   photo_url VARCHAR(255) NOT NULL
 );
---
+++ flybits_web_app/db/seeds/branches.sql
@@ -0,0 +1,1 @@
+INSERT INTO branches (owner_id, category_id, title, url, description) VALUES
